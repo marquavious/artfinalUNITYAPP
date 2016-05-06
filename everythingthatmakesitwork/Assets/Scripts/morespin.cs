38877fe37076ee627e77254e068cbb2c76811a27
marquavious <marq.draggon@gmail.com>
2016-05-06 16:25:13 -0400

    filesfilesfiles...andscripts...

---
+++ everythingthatmakesitwork/Assets/Scripts/morespin.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class morespin : MonoBehaviour {
+
+    public float speeda = -6f;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.up, speeda * Time.deltaTime);
+
+    }
+}
